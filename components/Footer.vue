64e404b6d91b2930365fcf4d08c2b3692726a779
katty azabache <kattyazabachez@gmail.com>
2020-04-24 21:12:14 -0500

    add footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,73 @@
 <template>
-  <div
-    class="w-full py-8 text-sm text-center md:text-left fade-in container mx-auto text-gray-500 text-lg p-4 lg:px-0"
-  >
-    No hay nada más fuerte en el mundo que el corazón de un voluntario.
-    <span class="font-bold"> Coronel James H. Doolittle</span>
+  <div class=" bg-gray-500">
+    <div
+      class="w-full py-8 text-sm text-center md:text-left fade-in container mx-auto text-white p-8 lg:flex flex-wrap"
+    >
+      <div class="lg:pr-8">
+        <p class="pb-2 font-bold">DESCARGAR</p>
+        <div class=" lg:flex flex-wrap">
+          <div class="pb-2 lg:pr-2">
+            <a
+              href="https://play.google.com/store/apps/details?id=org.banderablanca"
+              target="_blank"
+            >
+              <img
+                src="~/assets/img/Play Store.svg"
+                class="h-12 bounce-top-icons mx-auto"
+              />
+            </a>
+          </div>
+          <a href="https://apps.apple.com/pe/app/bandera-blanca/id1507865656">
+            <img
+              src="~/assets/img/App Store.svg"
+              class="h-12 lg:pr-4 bounce-top-icons mx-auto"
+            />
+          </a>
+        </div>
+      </div>
+      <div class="lg:pr-8">
+        <p class="pb-2 font-bold pt-8 lg:pt-0">INTEGRANTES</p>
+        <div class=" flex-wrap lg:flex">
+          <ul class="lg:pl-4">
+            <li class="font-light flex flex-wrap">
+              • Henry - <span class="font-bold"></span>
+            </li>
+            <li class="font-light flex flex-wrap">
+              • Angy Quispe - <span class="font-bold"></span>
+            </li>
+          </ul>
+          <ul class="lg:pl-6">
+            <li class="font-light flex flex-wrap">
+              • Edgar Huamaní - <span class="font-bold"></span>
+            </li>
+            <li class="font-light flex flex-wrap">
+              • Katherine Azabache - <span class="font-bold"></span>
+            </li>
+          </ul>
+          <ul class="lg:pl-6">
+            <li class="font-light flex flex-wrap">
+              • Johny Ronald - <span class="font-bold"></span>
+            </li>
+            <li class="font-light flex flex-wrap">
+              • Melanie - <span class="font-bold"></span>
+            </li>
+          </ul>
+          <ul class="lg:pl-6">
+            <li class="font-light flex flex-wrap">
+              • Guillermo - <span class="font-bold"></span>
+            </li>
+            <li class="font-light flex flex-wrap">
+              • Mario André - <span class="font-bold"></span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="bg-gray-800 p-4 text-center text-white">
+      <p>
+        No hay nada más fuerte en el mundo que el corazón de un voluntario.
+        <span class="font-bold"> Coronel James H. Doolittle</span>
+      </p>
+    </div>
   </div>
 </template>
