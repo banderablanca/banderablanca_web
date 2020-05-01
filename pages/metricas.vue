9f181d53b076b5b240aa3e7af94a443e1b888907
Edgar Huamani <ehuamani.arango@gmail.com>
2020-05-01 16:41:27 -0500

    changes metrics

--- pages/metricas.vue
+++ pages/metricas.vue
@@ -3,11 +3,20 @@
     <!--Nav-->
     <Nav />
     <iframe
-      class="w-5/6 mx-auto"
-      
+      class="w-5/6 mx-auto h-full"
       src="https://datastudio.google.com/embed/reporting/5f5e1e31-3f5e-4e40-9803-b79213374ecd/page/lPOOB"
       frameborder="0"
-      style="border:0;height:100vh;overflow-y:hidden !important"
+      style="border:0;overflow-y:hidden !important"
+      allowfullscreen
+      scrolling="no"
+    ></iframe>
+    <iframe
+      class="w-5/6 mx-auto h-full"
+      src="https://datastudio.google.com/embed/reporting/a7f73e40-8e1f-4edc-83c5-191605473e0f/page/lPOOB"
+      frameborder="0"
+      scrolling="no"
+      
+      style="border:0;overflow-y:hidden !important"
       allowfullscreen
     ></iframe>
     <!--Footer-->
